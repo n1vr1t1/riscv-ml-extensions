cfde4317381dd5cbc139d16ae12f279adea8c62b
n1vr1t1 <nivriti.raisingh@gmail.com>
2026-04-03 21:23:10 +0200

    reconfiguring signals

--- extending_riscv/extending_riscv.srcs/sources_1/new/execution_stage.vhd
+++ extending_riscv/extending_riscv.srcs/sources_1/new/execution_stage.vhd
@@ -6,17 +6,18 @@
 -- 
 -- Todo:
 -- 1. check if variable is working for the inputs of comparator and propagation of the signal to main alu
+-- 2. data and load hazards for vector instructions
 --------------------------------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity execution_stage is
-    Port( clk :  in std_logic;
-        rst :  in std_logic;
-        flush :  in std_logic;
+    Port( clk : in std_logic;
+        rst : in std_logic;
+        flush : in std_logic;
         source_1 : in STD_LOGIC_VECTOR( 31 downto 0 );
         source_2 : in STD_LOGIC_VECTOR( 31 downto 0 ); -- used in data memory and for vector load-store operations
-        value_3 : in STD_LOGIC_VECTOR( 31 downto 0 ); -- used only for mlu
+        source_3 : in STD_LOGIC_VECTOR( 31 downto 0 ); -- used only for mlu
         vec1_data : in std_logic_vector( 127 downto 0 );
         vec2_data : in std_logic_vector( 127 downto 0 );
         vec3_data : in std_logic_vector( 127 downto 0 );
@@ -34,22 +35,21 @@ entity execution_stage is
         alu_3_out : out STD_LOGIC_VECTOR( 31 downto 0 );
         alu_4_out : out STD_LOGIC_VECTOR( 31 downto 0 );
         branch_condition : out STD_LOGIC;
-        vec3_data : in std_logic_vector( 127 downto 0 );
         vec3_out : out std_logic_vector( 127 downto 0 );
         vec_we : in std_logic;
         vec_we_forward : out std_logic;
-        address_2 : in std_logic_vector(3 downto 0);
-        s_out : out std_logic_vector(3 downto 0);
-        p_in : in STD_LOGIC_VECTOR( 31 downto 0 );
-        p_out :  out STD_LOGIC_VECTOR( 31 downto 0 );
-        d_in :  in STD_LOGIC_VECTOR( 4 downto 0 );
-        d_out :  out STD_LOGIC_VECTOR( 4 downto 0 );
-        a_out : out std_logic;
-        b_out : out std_logic;
-        o_in : in STD_LOGIC_VECTOR( 4 downto 0 );
-        o_out : out STD_LOGIC_VECTOR( 4 downto 0 );
-        rs1_out : out STD_LOGIC_VECTOR( 31 downto 0 );
-        rs2_out : out STD_LOGIC_VECTOR( 15 downto 0 );
+        -- address_2 : in std_logic_vector(3 downto 0); -- ?
+        -- s_out : out std_logic_vector(3 downto 0);
+        pc_in : in STD_LOGIC_VECTOR( 31 downto 0 );
+        pc_out : out STD_LOGIC_VECTOR( 31 downto 0 );
+        dest_ad_in : in STD_LOGIC_VECTOR( 4 downto 0 );
+        dest_ad_out : out STD_LOGIC_VECTOR( 4 downto 0 );
+        a_sel_out : out std_logic;
+        b_sel_out : out std_logic;
+        opclass_in : in STD_LOGIC_VECTOR( 4 downto 0 );
+        opclass_out : out STD_LOGIC_VECTOR( 4 downto 0 );
+        source1_out : out STD_LOGIC_VECTOR( 31 downto 0 );
+        -- source2_out : out STD_LOGIC_VECTOR( 15 downto 0 );
         load_a : in STD_LOGIC;
         load_b : in STD_LOGIC;
         load_c : in std_logic;
@@ -58,9 +58,9 @@ end execution_stage;
 
 architecture Behavioral of execution_stage is
     component comparator is
-        Port( value_1 : in STD_LOGIC_VECTOR( 31 downto 0 );
-        value_2 : in STD_LOGIC_VECTOR( 31 downto 0 );
-        cond_opcode : in STD_LOGIC_VECTOR( 2 downto 0 );
+        Port( operand_1 : in STD_LOGIC_VECTOR (31 downto 0);
+        operand_2 : in STD_LOGIC_VECTOR (31 downto 0);
+        cond_opcode : in STD_LOGIC_VECTOR (2 downto 0);
         branch_condition : out STD_LOGIC );
     end component;
     component alu is
@@ -79,14 +79,14 @@ signal alu1_op_a, alu1_op_b, alu1_op_c : std_logic_vector( 31 downto 0 );
 signal conditional_opcode_signal : STD_LOGIC_VECTOR( 2 downto 0 );
 signal alu_opcode_signal : STD_LOGIC_VECTOR( 3 downto 0 );
 signal comp_a, comp_b : std_logic_vector( 31 downto 0 );
-signal alu2_op_a, alu2_op_b, alu2_op_c : std_logic_vector( 31 downto 0 );
-signal alu3_op_a, alu3_op_b, alu3_op_c : std_logic_vector( 31 downto 0 );
-signal alu4_op_a, alu4_op_b, alu4_op_c : std_logic_vector( 31 downto 0 );
+signal alu2_op_a, alu3_op_a, alu4_op_a : std_logic_vector( 31 downto 0 );
+-- signal alu2_op_b, alu3_op_b, alu4_op_b : std_logic_vector( 31 downto 0 );
+-- signal alu2_op_c, alu3_op_c, alu4_op_c : std_logic_vector( 31 downto 0 );
 
 begin
     comp_exe : comparator
-        Port map(value_1 => comp_a,
-                value_2 => comp_b,
+        Port map(operand_1 => comp_a,
+                operand_2 => comp_b,
                 cond_opcode => conditional_opcode_signal,
                 branch_condition => branch_condition );
     alu_1 : alu
@@ -101,8 +101,8 @@ begin
     alu_2 : alu
         Port map(opcode => alu_opcode_signal,
                 operand_1 => alu2_op_a,
-                operand_2 => alu2_op_b,
-                operand_3 => alu2_op_c,
+                operand_2 => vec2_data( 63 downto 32 ),
+                operand_3 => vec3_data( 63 downto 32 ),
                 is_float => is_float_signal,
                 is_ml => is_ml_signal,
                 en => vec_signal,
@@ -110,8 +110,8 @@ begin
     alu_3 : alu
         Port map(opcode => alu_opcode_signal,
                 operand_1 => alu3_op_a,
-                operand_2 => alu3_op_b,
-                operand_3 => alu3_op_c,
+                operand_2 => vec2_data( 95 downto 64 ),
+                operand_3 => vec3_data( 95 downto 64 ),
                 is_float => is_float_signal,
                 is_ml => is_ml_signal,
                 en => vec_signal,
@@ -119,8 +119,8 @@ begin
     alu_4 : alu
         Port map(opcode => alu_opcode_signal,
                 operand_1 => alu4_op_a,
-                operand_2 => alu4_op_b,
-                operand_3 => alu4_op_c,
+                operand_2 => vec2_data( 127 downto 96 ),
+                operand_3 => vec3_data( 127 downto 96 ),
                 is_float => is_float_signal,
                 is_ml => is_ml_signal,
                 en => vec_signal,
@@ -129,62 +129,76 @@ begin
 -- branch_condition <= branch_condition_signal;
 
 process ( rst, clk ) 
-variable post_load_a, post_load_b : std_logic_vector( 31 downto 0 );
+variable post_load_a, post_load_b, post_load_c : std_logic_vector( 31 downto 0 );
 begin
 	if rst = '0' then
-	    alu_opcode_signal <= ( others => '0' );
+	    alu_opcode_signal <= ( others => '1' );
 	    is_float_signal <= '0';
 	    is_ml_signal <= '0';
         vec_signal <= '0';
 	    conditional_opcode_signal <= ( others => '0' );
-        p_out <= ( others => '0' );
-		rs1_out <= ( others => '0' );
-		rs2_out <= ( others => '0' );
-		d_out <= ( others => '0' );
-		o_out <= ( others => '0' );
-		a_out <= '0';
-        b_out <= '0';
-        s_out <= (others => '0');
+        pc_out <= ( others => '0' );
+		source1_out <= ( others => '0' );
+--		source2_out <= ( others => '0' );
+		dest_ad_out <= ( others => '0' );
+		opclass_out <= ( others => '0' );
+		a_sel_out <= '0';
+        b_sel_out <= '0';
+        -- s_out <= (others => '0');
         vec3_out <= (others => '0');
 		alu1_op_a <= ( others => '0' );
 		alu1_op_b <= ( others => '0' );
 		alu1_op_c <= ( others => '0' );
         alu2_op_a <= ( others => '0' );
-        alu2_op_b <= ( others => '0' );
-        alu2_op_c <= ( others => '0' );
         alu3_op_a <= ( others => '0' );
-        alu3_op_b <= ( others => '0' );
-        alu3_op_c <= ( others => '0' );
         alu4_op_a <= ( others => '0' );
-        alu4_op_b <= ( others => '0' );
-        alu4_op_c <= ( others => '0' );
         vec_we_forward <= '0';
         post_load_a := ( others => '0' );
         post_load_b := ( others => '0' );
         comp_a <= ( others => '0' );
         comp_b <= ( others => '0' );
     elsif rising_edge( clk ) then
-        if flush ='0' then
-            a_out <= a_select( 0 );
-            b_out <= b_select( 0 );
+        if flush = '1' then
+            alu_opcode_signal <= ( others => '0' );
+            is_float_signal <= '0';
+            is_ml_signal <= '0';
+            vec_signal <= '0';
+            conditional_opcode_signal <= ( others => '0' );
+            pc_out <= ( others => '0' );
+            source1_out <= ( others => '0' );
+--            source2_out <= ( others => '0' );
+            dest_ad_out <= ( others => '0' );
+            opclass_out <= ( others => '0' );
+            a_sel_out <= '0';
+            b_sel_out <= '0';
+            -- s_out <= (others => '0');
+            vec3_out <= (others => '0');
+            alu1_op_a <= ( others => '0' );
+            alu1_op_b <= ( others => '0' );
+            alu1_op_c <= ( others => '0' );
+            alu2_op_a <= ( others => '0' );
+            alu3_op_a <= ( others => '0' );
+            alu4_op_a <= ( others => '0' );
+            vec_we_forward <= '0';
+            post_load_a := ( others => '0' );
+            post_load_b := ( others => '0' );
+            comp_a <= ( others => '0' );
+            comp_b <= ( others => '0' );
+	    else
+            a_sel_out <= a_select( 0 );
+            b_sel_out <= b_select( 0 );
             alu_opcode_signal <= alu_opcode;
             is_float_signal <= is_float;
 	        is_ml_signal <= is_ml;
             vec_signal <= is_vec;
             vec_we_forward <= vec_we;
             conditional_opcode_signal <= conditional_opcode;
-            p_out <= p_in;
-            rs1_out <= source_1;
-            rs2_out <= value_2( 15 downto 0 );
-            d_out <= d_in;
-            o_out <= o_in;
+            pc_out <= pc_in;
+            source1_out <= source_1;
+--            source2_out <= source_2( 15 downto 0 );
+            dest_ad_out <= dest_ad_in;
+            opclass_out <= opclass_in;
             vec3_out <= vec3_data;
-            alu2_op_b <= vec2_data( 63 downto 32 );
-            alu2_op_c <= vec3_data( 63 downto 32 );
-            alu3_op_b <= vec2_data( 95 downto 64 );
-            alu3_op_c <= vec3_data( 95 downto 64 );
-            alu4_op_b <= vec2_data( 127 downto 96 );
-            alu4_op_c <= vec3_data( 127 downto 96 );
             if load_a = '1' then  --  load hazard for 32 bit data
                 post_load_a := memory_value;
             else
@@ -193,7 +207,7 @@ begin
             if load_b = '1' then --load hazard for 32 bit data
                 post_load_b := memory_value;
             else 
-                post_load_b := value_2;
+                post_load_b := source_2;
             end if;
             comp_a <= post_load_a;
             comp_b <= post_load_b;
@@ -204,7 +218,7 @@ begin
                     -- alu3_op_a <= post_load_a;
                     -- alu4_op_a <= post_load_a;
                 when "01" => -- program counter
-                    alu1_op_a <= p_in;
+                    alu1_op_a <= pc_in;
                     alu1_op_a <= ( others => '0' );
                     alu2_op_a <= ( others => '0' );
                     alu3_op_a <= ( others => '0' );
@@ -226,7 +240,7 @@ begin
                     alu4_op_a <= post_load_a;
             end case;
             case b_select is -- selecting operand 2 based on b_select 
-                -- when "00" => alu1_op_b <= value_2;
+                -- when "00" => alu1_op_b <= source_2;
                 when "01" => -- immediate (only for the main alu)
                     alu1_op_b <= immediate;
                 when "10" => -- value from vector register
@@ -239,40 +253,8 @@ begin
             elsif load_c = '1' then
                 alu1_op_c <= memory_value; -- load hazard
             else
-                alu1_op_c <= value_3;
+                alu1_op_c <= source_3;
             end if;
-	    else
-            alu_opcode_signal <= ( others => '0' );
-            is_float_signal <= '0';
-            is_ml_signal <= '0';
-            vec_signal <= '0';
-            conditional_opcode_signal <= ( others => '0' );
-            p_out <= ( others => '0' );
-            rs1_out <= ( others => '0' );
-            rs2_out <= ( others => '0' );
-            d_out <= ( others => '0' );
-            o_out <= ( others => '0' );
-            a_out <= '0';
-            b_out <= '0';
-            s_out <= (others => '0');
-            vec3_out <= (others => '0');
-            alu1_op_a <= ( others => '0' );
-            alu1_op_b <= ( others => '0' );
-            alu1_op_c <= ( others => '0' );
-            alu2_op_a <= ( others => '0' );
-            alu2_op_b <= ( others => '0' );
-            alu2_op_c <= ( others => '0' );
-            alu3_op_a <= ( others => '0' );
-            alu3_op_b <= ( others => '0' );
-            alu3_op_c <= ( others => '0' );
-            alu4_op_a <= ( others => '0' );
-            alu4_op_b <= ( others => '0' );
-            alu4_op_c <= ( others => '0' );
-            vec_we_forward <= '0';
-            post_load_a := ( others => '0' );
-            post_load_b := ( others => '0' );
-            comp_a <= ( others => '0' );
-            comp_b <= ( others => '0' );
         end if;
 	end if;
 end process;
